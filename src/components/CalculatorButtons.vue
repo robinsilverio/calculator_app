9635fa47a5565a7164b06e0fae606d99cc323300
Robinsilverio <robinsilverio17@gmail.com>
2021-10-24 13:12:10 +0200

    Finished styling buttons.

--- src/components/CalculatorButtons.vue
+++ src/components/CalculatorButtons.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="calculator_main">
         <div class="calculator_buttons">
-            <button v-for="button in buttons.slice(0, 20)" :key="button.text">{{ button.text }}</button>
+            <button v-for="button in buttons.slice(0, 20)" :class="button.className" :key="button.text">{{ button.text }}</button>
             <div class="last_row_buttons">
-                <button v-for="button in buttons.slice(20)" :key="button.text">{{ button.text }}</button>
+                <button v-for="button in buttons.slice(20)" :class="button.className" :key="button.text">{{ button.text }}</button>
             </div>
         </div>
     </div>
@@ -31,19 +31,19 @@ export default {
                     text: 'M+'
                 }, 
                 {
-                    className: 'btn_reg',
+                    className: 'btn_regular',
                     text: '÷'
                 }, 
                 {
-                    className: 'btn_reg',
+                    className: 'btn_regular',
                     text: '%'
                 }, 
                 {
-                    className: 'btn_reg',
+                    className: 'btn_regular',
                     text: 7
                 }, 
                 {
-                    className: 'btn_reg',
+                    className: 'btn_regular',
                     text: 8
                 }, 
                 {
@@ -116,21 +116,22 @@ export default {
 }
 </script>
 <style scoped>
+@import url('../assets/stylesheets/buttons.css');
 .calculator_main { 
     margin-top: 10px;
 }
+.calculator_buttons, .last_row_buttons { display: flex; }
 .calculator_buttons {
-    display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
     padding: 5px;
 }
 .calculator_buttons .last_row_buttons {
-    display: flex;
     flex: auto;
 }
 .calculator_buttons button {
     flex: 0 20%;
     padding: 15px;
+    color: #FFF;
 }
 </style>

97cba43e70f444dbbbb2c982dadab9bd3a9314d4
Robinsilverio <robinsilverio17@gmail.com>
2021-10-24 14:15:24 +0200

    Encapsulated buttonData

--- src/components/CalculatorButtons.vue
+++ src/components/CalculatorButtons.vue
@@ -9,108 +9,12 @@
     </div>
 </template>
 <script>
+import { buttonData } from "../data/buttonData.js";
 export default {
     name: 'CalculatorButtons',
     data() {
         return {
-            buttons: [
-                {
-                    className: 'btn_regular btn_off',
-                    text: 'OFF'
-                }, 
-                {
-                    className: 'btn_mem',
-                    text: 'MRC'
-                }, 
-                {
-                    className: 'btn_mem',
-                    text: 'M-'
-                }, 
-                {
-                    className: 'btn_mem',
-                    text: 'M+'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: '÷'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: '%'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 7
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 8
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 9
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 'x'
-                },
-                {
-                    className: 'btn_regular',
-                    text: '√'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 4
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 5
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 6
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: '-'
-                },
-                {
-                    className: 'btn_clear',
-                    text: 'CE'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 1
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 2
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 3
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: '+'
-                },
-                {
-                    className: 'btn_clear',
-                    text: 'AC'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: 0
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: '.'
-                }, 
-                {
-                    className: 'btn_regular',
-                    text: '='
-                }
-            ]
+            buttons: buttonData
         }
     }
 }

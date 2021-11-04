069c34e99aad07b6e76ae99bfcec400dcb243462
Robinsilverio <robinsilverio17@gmail.com>
2021-11-04 17:23:46 +0100

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
+# abort on errors
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m "New deployment"
+git commit -m 'deploy'
+
 git push -f git@github.com:robinsilverio/calculator_app.git master:gh-pages
 
 cd -
